353abcd4c920a0a7fbf5687e78e3b708b7bb1390
Angel RD <98786236+RDangellons@users.noreply.github.com>
2025-11-22 16:53:05 -0600

    Creacion de conexion
    Creacion de variables para la conexion de la base de datos (hostinguer)

--- config/db.php
+++ config/db.php
@@ -1,11 +1,17 @@
 <?php
 
 header('Content-Type: application/json; charset=utf-8');
-
+/* conexion al xampp
 $host= "localhost";
 $dbname= "taller_carlup";
 $username="root"; //predeterminado de xampp pero cambia en un hosting 
 $password=""; //predeterminado de xampp pero cambia en un hosting
+*/
+// conexion al servidor de hostinguer
+$host= "localhost";
+$dbname= "u190296732_taller_carlup";
+$username="u190296732_iscangel"; //predeterminado de xampp pero cambia en un hosting 
+$password="Angelalonso45#"; //predeterminado de xampp pero cambia en un hosting
 
 try{
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username,$password);
